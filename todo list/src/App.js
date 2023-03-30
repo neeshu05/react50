a71dd93aa9953e93238f14d4bd515b8aa83f9af6
neeshu maini <97976949+neeshu05@users.noreply.github.com>
2023-03-30 12:15:49 +0530

    Create App.js

---
+++ todo list/src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import Form from './components/Form'
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Todo list </h1>
+      <Form /> </div>
+  );
+}
